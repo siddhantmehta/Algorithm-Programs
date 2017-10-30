6d23a9df3682af759b2bd99fa8d9b4873ddb1794
siddhantmehta <siddhantmehta194@gmail.com>
2017-10-30 20:21:42 +0530

    Longest Palindrome Subsequence code

---
+++ dynamicProgramming/longestPalindromeSubsequence.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	char *s=(char *)malloc(sizeof(char)*100000);
+	printf("Enter string:");
+	scanf("%s",s);
+	int len=strlen(s);
+	int dpTable[len][len];
+	memset(dpTable,0,sizeof(dpTable[0][0])*len*len);
+	for(int i=0;i<len;i++)
+	{
+		dpTable[i][i]=1;
+	}
+	for(int i=0;i<len-1;i++)
+	{
+		if(s[i]==s[i+1])
+		{
+			dpTable[i][i+1]=2;
+		}
+		else
+		{
+			dpTable[i][i+1]=1;
+		}
+	}
+/*	for(int i=0;i<len;i++)
+	{
+		printf("%d\n",dpTable[i][i]);
+	}
+	printf("---------------------------------\n");
+	for(int i=0;i<len-1;i++)
+	{
+		printf("%d\n",dpTable[i][i+1]);
+	}
+	*/
+	int l=2;
+	int i,j;
+	while(l<len)
+	{
+		i=0;
+		j=l;
+		while(i<=len-1-l)
+		{
+			if(s[i]==s[j])
+			{
+				dpTable[i][j]=2+dpTable[i+1][j-1];
+			}
+			else
+			{
+				if(dpTable[i+1][j]>dpTable[i][j-1])
+				{
+					dpTable[i][j]=dpTable[i+1][j];
+				}
+				else
+				{
+					dpTable[i][j]=dpTable[i][j-1];
+				}
+			}
+			j++;
+			i++;
+		}
+		l++;
+	}
+	i=0;
+	while(i<len)
+	{
+		l=0;
+		while(l<len)
+		{
+			printf("%d ",dpTable[i][l]);
+			l++;
+		}
+		printf("\n");
+		i++;
+	}
+	printf("Length of longest common palindrome subsequence :%d\n",dpTable[0][len-1]);
+	int temp=dpTable[0][len-1];
+	char * subsequence=(char *)malloc(sizeof(char)*temp+1);
+	subsequence[temp]='\0';
+	temp--;
+	j=len-1;
+	i=0;
+	while(i!=j)
+	{
+		if(s[i]==s[j])
+		{
+			subsequence[temp]=s[j];
+			temp--;
+			i++;
+			j--;
+		}
+		else
+		{
+			j--;
+		}
+	}
+	while(j>=0)
+	{
+		subsequence[temp]=s[j];
+		j--;
+		temp--;
+	}
+	printf("%s\n",subsequence);
+	return 0;
+}
